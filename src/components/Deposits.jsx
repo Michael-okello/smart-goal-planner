cc01f615a471206bf761762d836757ecbb986502
Michael-okello <michjosepzzz@gmail.com>
2025-07-24 15:38:32 +0300

    form for deposits

---
+++ src/components/Deposits.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function Deposit() {
+    function addDeposits(formData) {
+    const deposit = formData.get("deposit")
+    }
+    return(
+        <section>
+            <h1>Deposits</h1>
+            <form method="post">
+            <label htmlFor="deposit">Deposit</label>
+            <input id="deposit" type="number" name="deposit" />
+            </form>
+        </section>
+    )
+}
